9925b8463b7c76ffa0392a4c5b5365611f5c72df
JordanLong1 <jordanleelong5@gmail.com>
2026-04-11 23:12:07 -0700

    removes comment from skeleton

--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -2,12 +2,6 @@ interface SkeletonProps {
   className?: string;
 }
 
-/**
- * A shimmering placeholder block. Compose multiple of these to build
- * skeleton screens that mirror your real layout.
- *
- * Usage: <Skeleton className="h-6 w-32" />
- */
 export function Skeleton({ className = '' }: SkeletonProps) {
   return (
     <div
